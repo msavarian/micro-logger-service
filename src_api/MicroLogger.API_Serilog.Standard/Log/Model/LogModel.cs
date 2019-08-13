3a3ea5d0ce77b5db37c4acf6b108146a2d9ae914
Mahmoud Savarian (IT10075) <m-savarian@outlook.com>
2019-08-13 11:24:30 +0430

    init

---
+++ src_api/MicroLogger.API_Serilog.Standard/Log/Model/LogModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroLogger.API_Serilog.Standard.Log.Model
+{
+    public class LogModel
+    {
+        public string Message { get; set; }
+        public LogType LogType { get; set; }
+        public DateTime LogDateTime { get; set; }
+
+    }
+}
---
+++ src_api/MicroLogger.API_Serilog.Standard/Log/Model/LogType.cs
@@ -0,0 +1,21 @@
+﻿namespace MicroLogger.API_Serilog.Standard.Log.Model
+{
+    /// <summary>
+    /// نوع لاگ
+    /// </summary>
+    public enum LogType
+    {
+        Custom_LOG = 1,
+
+        Security_LOG = 2,
+
+        LoginAudit_LOG = 3,
+
+        Application_LOG = 4,
+
+        Server_LOG = 5,
+
+        DatabaseCommands_LOG = 6
+
+    }
+}
