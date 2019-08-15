0aaa785f9dbac1509ed832540b860afa46b7a8fe
Mahmoud Savarian (Personal Laptop) <sav68.net@gmail.com>
2019-08-15 17:54:12 +0430

    fix

---
+++ src_api/MicroLogger.API.SerilogToRabbitMQ.Standard/Log/Base/ILogBase.cs
@@ -0,0 +1,9 @@
+﻿using MicroLogger.API_Serilog.Standard.Log.Model;
+
+namespace MicroLogger.API_Serilog.Standard.Log.Base
+{
+    public interface ILogBase
+    {
+        void Log(LogModel log);
+    }
+}
