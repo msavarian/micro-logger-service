fd9086f99dfc5da57dc838188a19d354eb5cb6a7
Mahmoud Savarian (Personal Laptop) <sav68.net@gmail.com>
2019-08-15 17:54:15 +0430

    fix

--- src_api/MicroLogger.API_Serilog.Standard/Log/Base/ILogBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using MicroLogger.API_Serilog.Standard.Log.Model;
-
-namespace MicroLogger.API_Serilog.Standard.Log.Base
-{
-    public interface ILogBase
-    {
-        void Log(LogModel log);
-    }
-}
